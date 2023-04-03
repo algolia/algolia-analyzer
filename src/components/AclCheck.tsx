4a52c46a30405fd18a3c360a9f9625a903068e26
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2023-04-03 16:36:37 +0200

    feat: allow to get settings

--- src/components/AclCheck.tsx
+++ src/components/AclCheck.tsx
@@ -1,19 +1,32 @@
-import type { ApiKeyACLType } from '@algolia/client-search';
-import { Badge, Card, IconButton, Input } from '@algolia/satellite';
+import type { ApiKeyACLType, Settings } from '@algolia/client-search';
+import { Alert, Badge, Button, Card, IconButton, Input } from '@algolia/satellite';
 import algoliasearch from 'algoliasearch';
 import { type FC, useCallback, useEffect, useState, useTransition } from 'react';
-import { Slash } from 'react-feather';
+import { Slash, Settings as Cog } from 'react-feather';
+
+import { Code } from './Code';
 
 export const AclCheck: FC = () => {
   const [pending, startTransition] = useTransition();
   const [appId, setAppId] = useState<string>();
   const [apiKey, setApiKey] = useState<string>();
+  const [indexName, setIndexName] = useState<string>();
+  const [settings, setSettings] = useState<Settings | null>(null);
   const [acl, setAcl] = useState<ApiKeyACLType[]>([]);
 
   const resetInput = useCallback(() => {
     setAppId('');
     setApiKey('');
+    setIndexName('');
+    setSettings(null);
   }, []);
+
+  const getIndexSettings = async (): Promise<void> => {
+    if (!appId || !apiKey || !indexName) return;
+    const client = algoliasearch(appId, apiKey);
+    const index = client.initIndex(indexName);
+    setSettings(await index.getSettings());
+  };
 
   useEffect(() => {
     if (!appId || !apiKey) {
@@ -38,46 +51,76 @@ export const AclCheck: FC = () => {
   }, [appId, apiKey]);
 
   return (
-    <Card className="mx-2 mb-2 space-y-2">
-      <div className="flex items-center space-x-2">
-        <div>Check ACL: </div>
-        <Input
-          type="text"
-          id="AppId"
-          placeholder="Application Id"
-          className="w-[7.5rem]"
-          value={appId}
-          onChange={(e): void => setAppId(e.currentTarget.value)}
-        />
-        <Input
-          type="text"
-          id="ApiKey"
-          placeholder="API Key"
-          className="w-[17.5rem]"
-          value={apiKey}
-          onChange={(e): void => setApiKey(e.currentTarget.value)}
-        />
-        <IconButton
-          icon={Slash}
-          variant="subtle"
-          size="large"
-          title="clear inputs"
-          disabled={!appId && !apiKey}
-          onClick={resetInput}
-        />
-      </div>
-      <div className="space-x-1 space-y-1">
-        {!pending && acl.length > 0 ? (
-          <>
-            ACL ({acl.length}):{' '}
-            {acl.map((value) => (
-              <Badge value={value} key={value} />
-            ))}
-          </>
-        ) : (
-          'no ACL 🙅'
+    <div className="mx-2 space-y-4">
+      <Card className="space-y-2">
+        <div className="flex items-center space-x-2">
+          <div>Check ACL: </div>
+          <Input
+            type="text"
+            id="AppId"
+            placeholder="Application Id"
+            className="w-[7.5rem]"
+            value={appId}
+            onChange={(e): void => setAppId(e.currentTarget.value)}
+          />
+          <Input
+            type="text"
+            id="ApiKey"
+            placeholder="API Key"
+            className="w-[17.5rem]"
+            value={apiKey}
+            onChange={(e): void => setApiKey(e.currentTarget.value)}
+          />
+          <IconButton
+            icon={Slash}
+            variant="subtle"
+            size="large"
+            title="clear inputs"
+            disabled={!appId && !apiKey}
+            onClick={resetInput}
+          />
+        </div>
+        <div className="space-x-1 space-y-1">
+          {!pending && acl.length > 0 ? (
+            <>
+              ACL ({acl.length}):{' '}
+              {acl.map((value) => (
+                <Badge value={value} key={value} />
+              ))}
+            </>
+          ) : (
+            'no ACL 🙅'
+          )}
+        </div>
+      </Card>
+      <Card className="space-y-2">
+        {!acl.includes('settings') && (
+          <Alert usageContext="section">
+            Your ACL must contain <Badge value="settings" /> to get settings.
+          </Alert>
         )}
-      </div>
-    </Card>
+        <div className="flex items-center space-x-2">
+          <Input
+            type="text"
+            id="indexName"
+            placeholder="Index Name"
+            className="w-[17.5rem]"
+            value={indexName}
+            onChange={(e): void => setIndexName(e.currentTarget.value)}
+          />
+          <Button
+            startIcon={Cog}
+            variant="neutral"
+            size="large"
+            title="get index settings"
+            disabled={!indexName || !acl.includes('settings')}
+            onClick={getIndexSettings}
+          >
+            get settings
+          </Button>
+        </div>
+        <Code code={settings} leftPadding={false} />
+      </Card>
+    </div>
   );
 };
--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,13 +1,15 @@
+import cx from 'classnames';
 import type { FC } from 'react';
 import JsonView from 'react-json-view';
 
 interface CodeProps {
   code?: unknown;
+  leftPadding?: boolean;
 }
 
-export const Code: FC<CodeProps> = ({ code }) =>
+export const Code: FC<CodeProps> = ({ code, leftPadding = true }) =>
   code ? (
-    <div className="pl-6 py-2 break-all overflow-x-hidden">
+    <div className={cx('py-2 break-all overflow-x-hidden', leftPadding && 'pl-6')}>
       <JsonView
         src={code as Record<string, unknown>}
         name={null}
