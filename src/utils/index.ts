b9f3c9e0b9930eb0cd2c36fe74f64c040d3ab1d7
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2023-11-06 20:42:05 +0100

    feat: add isDefined util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './constants';
 export * from './getUrlData';
+export * from './isDefined';
 export * from './localStorage';
 export * from './Request';
 export * from './safeJsonParse';
