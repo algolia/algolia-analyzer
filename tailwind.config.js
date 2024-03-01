717614269d3017eb06c0cea645ddbf994e36e47f
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2024-03-01 12:42:41 +0100

    chore: fix satellite css missing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-const sourceSatellite = require('@algolia/satellite/cjs/styles/tailwind.config.js');
+const sourceSatellite = require('@algolia/satellite/tailwind.config.js');
 
 const { prefix, ...satellite } = sourceSatellite;
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [satellite],
-  content: ['./src/**/*.html', './src/**/*.ts', './src/**/*.tsx'],
+  content: ['./node_modules/@algolia/satellite/**/*.js', './src/**/*.{html,ts,tsx}'],
   theme: {
     ...satellite.theme,
     extends: {},
